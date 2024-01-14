5bc69e185ba5c070ca08411f8457865666498207
Badr El Mazaz <badr.elmazaz@eng.it>
2024-01-14 23:07:58 +0100

    new lc problem solved

---
+++ Leetcode/119. Pascal's Triangle II/Solutions/py/1/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        # n = rowIndex
+        # Time Complexity:  O(n)
+        # Space Complexity: O(n)
+
+        if rowIndex == 0:
+            return [1]
+
+        elif rowIndex == 1:
+            return [1, 1]
+
+        last_row = [1, 1]
+
+        for i in range(rowIndex - 1):
+            current_row = [1]
+
+            for j in range(i + 1):
+                current_value = last_row[j]
+                next_value = last_row[j + 1]
+                current_row.append(current_value + next_value)
+                
+            current_row.append(1)
+            last_row = current_row
+
+        return current_row
+        
