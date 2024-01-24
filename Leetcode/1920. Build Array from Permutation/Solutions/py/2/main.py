42ad15126c648b75ffa59bdf03a552d02135724d
Badr El Mazaz <badr.elmazaz@eng.it>
2024-01-24 20:54:20 +0100

    solved lc #1920 update

--- Leetcode/1920. Build Array from Permutation/Solutions/py/2/main.py
+++ Leetcode/1920. Build Array from Permutation/Solutions/py/2/main.py
@@ -1,14 +1,19 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-       # n = len(nums)
-       # Time Complexity:  O(n)
-       # Space Complexity: O(1)
 
        n = len(nums)
 
        for i in range(len(nums)):
-           nums[i] += n * (nums[nums[i]] % n)
-
+           # hide 2 numbers ('a' and 'b') in one number ('x') and use module to get 'b' and division to get 'a' from 'x'
+           # x = a + b * n
+           # b = x % n
+           # a = x // n
+           a = nums[i]
+           b = nums[a]
+           # if b is greater than n it means it was changed so use module to get the original number
+           if b >= n:
+                b %= n
+           nums[i] = a + b * n
 
        for i in range(len(nums)):
             nums[i] //= n
